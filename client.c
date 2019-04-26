b064acf72dcd4b5f8ba1d3a7bc103138c2614eda
MK-1111 <spacetowndream999@yahoo.co.jp>
2019-04-26 10:21:03 +0900

    20190426

--- client.c
+++ client.c
@@ -1,6 +1,19 @@
 #include<stdio.h>
 #include<sys/socket.h>
 #include<arpa/inet.h>
+#include<unistd.h>
+#include<string.h>
+
+void commun(int sock){
+	char buf[256];
+	int len_r;
+    char *message="Hello Server!!";
+	send(sock,message,strlen(message),0);
+	len_r=recv(sock,buf,256,0);
+	buf[len_r]='\0';
+	printf("%s\n",buf);
+}
+
 int main(int argc,char **argv){
 	int sock=socket(PF_INET,SOCK_STREAM,0);
 	
@@ -11,6 +24,8 @@ int main(int argc,char **argv){
 	target.sin_port=htons(10001);
 	
 	connect(sock,(struct sockaddr*)&target,sizeof(target));
+	
+	commun(sock);
 	
 	close(sock);
 	return 0;
