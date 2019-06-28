3a468c71171e1067663e1544885ac6636c846020
4428_村上 洸太 <k16191km>
2019-06-28 10:35:40 +0900

    20190628

--- www/server.c
+++ www/server.c
@@ -13,15 +13,39 @@ void DieWithError(char *errorMessage){
 
 void commun(int sock){
 char buf[BUF_SIZE];
+char response[BUF_SIZE];
 	int len_r;
-	len_r=recv(sock,buf,BUF_SIZE,0);
+    while(len_r=recv(sock,buf,BUF_SIZE,0)>0){
+	buf[len_r]='\0';
+    printf("%s\n",buf);
+    if(strstr(buf,"\r\n\r\n")){
+        break;
+    }
+    }
+
 	if(len_r<=0)
 	DieWithError("recv() failed");
-	buf[len_r]='\0';
-	printf("%s\n",buf);
-    if(send(sock,buf,strlen(buf),0)!=strlen(buf))
+
+    printf("received HTTP Request\n");
+
+    sprintf(response,"HTTP1.1/ 200 OK\r\n");
+
+
+    if(send(sock,response,strlen(response),0)!=strlen(response))
+    DieWithError("send() sent a message of unexpected bytes");
+
+    sprintf(response,"Content-Type:text/html\r\n");
+
+    if(send(sock,response,strlen(response),0)!=strlen(response))
+    DieWithError("send() sent a message of unexpected bytes");
+
+    sprintf(response,"<html>\r\n");
+
+      if(send(sock,response,strlen(response),0)!=strlen(response))
     DieWithError("send() sent a message of unexpected bytes");
 }
+
+
 
 int main(int argc,char **argv){
     int servSock=socket(PF_INET,SOCK_STREAM,0);
